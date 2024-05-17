f3247af683e6c3357238a6bfbaa9bcce39b7f5e6
Sahra012 <sfournet@iut.univ-paris8.fr>
2024-05-17 11:15:22 +0200

    J'ai fait l'héritage linkVue, cowboyVue et DragonVue

---
+++ src/main/java/universite_paris8/iut/lefarwestenperil/sae2_04/Vue/LinkVue.java
@@ -0,0 +1,23 @@
+package universite_paris8.iut.lefarwestenperil.sae2_04.Vue;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import universite_paris8.iut.lefarwestenperil.sae2_04.Modele.Personnage;
+import universite_paris8.iut.lefarwestenperil.sae2_04.Modele.Terrain;
+
+public class LinkVue extends PersonnageVue {
+
+
+    public LinkVue(Terrain terrain, Pane panneauDeJeu) {
+        super(terrain, panneauDeJeu);
+    }
+
+    @Override
+    public void creerPersonnage(Personnage personnage) {
+        Circle circle = new Circle(7, Color.RED);
+        circle.translateXProperty().bind(personnage.xProperty());
+        circle.translateYProperty().bind(personnage.yProperty());
+        super.getPanneauDeJeu().getChildren().add(circle);
+    }
+}
